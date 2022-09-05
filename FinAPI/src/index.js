7410e8a4d7c8565dcd37deb59364b52d18c64940
silas de paula <silasandrade94@gmail.com>
2022-09-05 18:24:25 -0300

    estrato por data

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -86,10 +86,9 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res)=>{
 
     const statementOperation = {
         description: description,
-        amount: amount,
+        amount: description,
         created_at: new Date(),
-        type: "credit"
-
+        type: "credit",
     }
 
     customer.statement.push(statementOperation);
@@ -124,8 +123,21 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (req, res)=>{
     return res.status(201).json({
         "msg": "Saque efetuado con sucesso!"
     })
-})
+});
+
+app.get('/statement/date', verifyIfExistsAccountCPF, (req, res)=>{
+    const { customer} = req;
+    const { date } = req.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter((statement)=> statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+
+  
+    return res.json(statement) // Buscando Estrado
+});
 
 
 
-app.listen(3333);
+app.listen(3333)
