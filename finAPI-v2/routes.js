83133409b10b778702aaadbfec1f86cb064d9e2a
Silas de Paula <silasandrade94@gmail.com>
2022-10-10 20:18:18 -0300

    v2

--- finAPI-v2/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const createAccount = require('./src/controllers/createAcoont')
-
-const routes = express.Router()
-
-routes.post('/account', createAccount.createUser)
-
-module.exports = routes
