8e3a08bd98c52b0b032c90ae74669203e308eb5b
SilasEduardo <silasandrade94@gmail.com>
2022-11-15 22:37:42 -0300

    uuid

--- Cap2/Rentalx/src/server.ts
+++ Cap2/Rentalx/src/server.ts
@@ -1,18 +1,9 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 const app = express()
-
 app.use(express.json())
+app.use("/categories", categoriesRoutes)
 
-app.get('/', (req, res) => {
-    res.send('deu certo')
-});
-
-app.post("/courses", (req, res)=>{
-    console.log(req.body)
-    const {course} = req.body
-
-    res.status(201).json({course})
-})
 
 app.listen(3333, ()=>{
     console.log('SERVER ON')
