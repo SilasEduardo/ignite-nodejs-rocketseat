dd819d78949a7c60969d24a656be5fb1d05be07a
Silas Eduardo <54821901+SilasEduardo@users.noreply.github.com>
2022-11-28 22:40:05 -0300

    Update routes.js

--- finAPI-v2/routes.js
+++ finAPI-v2/routes.js
@@ -5,8 +5,8 @@ const accountCotroller = require('./src/controllers/account.controller')
 routes.use(express.json())
 
 
-routes.post('/account', accountCotroller.createAccount)
-routes.get('/statement/:id', accountCotroller.statement)
+routes.post('/account', accountCotroller.createAccount) //controllers 
+routes.get('/statement/:id', accountCotroller.statement)// controller
 
 
 module.exports = routes;
