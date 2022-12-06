ee91cf6a9311b96dcf587da3038569cd6f665ff0
SilasEduardo <silasandrade94@gmail.com>
2022-12-06 17:40:12 -0300

    CRUD

--- Cap2/Rentalx/src/routes/categoriesRoutes.ts
+++ Cap2/Rentalx/src/routes/categoriesRoutes.ts
@@ -1,27 +1,11 @@
-import {  Router } from "express";
-import { CategoriesRepositores } from "../repositories/CategoriesRepositories";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { Router} from "express";
 
-const categoriesRepositores = new CategoriesRepositores()
-const categoriesRoutes = Router();
-
+const categoriesRoutes = Router()
 
 
 categoriesRoutes.post("/", (request, response) => {
-    const {name, description} = request.body;
-    const createCategoriesService = new CreateCategoryService(categoriesRepositores);
-    createCategoriesService.execute({name, description})
     
-    return response.status(201).send()
-
 });
 
 
-categoriesRoutes.get("/", (request, response) => {
-    const category = categoriesRepositores.list()
-
-    response.status(201).json(category)
-})
-
-
-export { categoriesRoutes }
+export {categoriesRoutes}
--- Cap2/Rentalx/src/server.ts
+++ Cap2/Rentalx/src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
-import { categoriesRoutes } from './routes/categoriesRoutes';
+import  express from "express";
+import { categoriesRoutes } from "./routes/categoriesRoutes";
+
 const app = express()
-app.use(express.json());
-app.use("/categories", categoriesRoutes);
+app.use(express.json())
+app.use("/categories", categoriesRoutes)
 
-
-
-app.listen(3333, () => {
-    console.log('server on');
-});
+app.listen(3000, () => {
+    console.log("SERVER ON")
+})
