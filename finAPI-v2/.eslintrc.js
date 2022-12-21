bb1d345aa6db8893ec783d60271024e9505328d3
SilasEduardo <silasandrade94@gmail.com>
2022-12-20 23:54:45 -0300

    modelando

---
+++ finAPI-v2/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'import/first': 'off',
+    'import/newline-after-import': 'off',
+    'quote-props': 'off',
+  },
+};
