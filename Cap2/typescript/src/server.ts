511e4776b2f7953e51eeb883050bcb8174474ca4
SilasEduardo <silasandrade94@gmail.com>
2023-01-05 14:07:31 -0300

    teste

--- Cap2/typescript/src/server.ts
+++ Cap2/typescript/src/server.ts
@@ -2,9 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("Cheguei agora");
 });
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  res.status(201).json(name);
+});
+
 app.listen(3333, () => {
   console.log("Server on");
 });
